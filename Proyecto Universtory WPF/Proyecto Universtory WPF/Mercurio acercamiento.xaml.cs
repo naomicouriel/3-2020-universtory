e7d0810f351c05ca5efe99e82822757bcf83cf62
naomicouriel <naomicouriel@gmail.com>
2020-10-29 12:52:22 -0300

    Correción de errores
    Arreglé los errores y FUNCIONAN TODOS LOS BOTONES Y LAS PÁGINAS DE DATOS 😁

--- Proyecto Universtory WPF/Proyecto Universtory WPF/Mercurio acercamiento.xaml.cs
+++ Proyecto Universtory WPF/Proyecto Universtory WPF/Mercurio acercamiento.xaml.cs
@@ -25,13 +25,14 @@ namespace Proyecto_Universtory_WPF
             InitializeComponent();
         }
 
-        private void VolMerbtn_Click(object sender, RoutedEventArgs e)
+        private void VolMerbtn_Click_1(object sender, RoutedEventArgs e)
         {
             MainWindow obj = (MainWindow)Window.GetWindow(this);
             obj.frame.Content = new Page1();
+
         }
 
-        private void DatMerbtn_Click(object sender, RoutedEventArgs e)
+        private void DatMerbtn_Click_1(object sender, RoutedEventArgs e)
         {
             Datos_Mercurio datMer = new Datos_Mercurio();
             datMer.ShowDialog();
