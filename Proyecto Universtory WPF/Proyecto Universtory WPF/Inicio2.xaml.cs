7c06562a56d07cb08f3778ff1e0d23bd09c4f777
naomicouriel <naomicouriel@gmail.com>
2020-11-03 03:38:43 -0300

    música y efectos de objetos
    Agregué dos animaciones a los botones de la pantalla de inicio, le cambié el style al botón de volver que está en la pantalla de principal planetas y agregué la música con un botoncito para frenarla. Falta poner que vuelva a empezar en loop, además de poder pausarla y continuar (pero no tiene que estar en el form de la page de inicio -la música-, sino en la MainWindow -el botoncito para frenarla si puede estar solo en la page-)

--- Proyecto Universtory WPF/Proyecto Universtory WPF/Inicio2.xaml.cs
+++ Proyecto Universtory WPF/Proyecto Universtory WPF/Inicio2.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,6 +26,10 @@ namespace Proyecto_Universtory_WPF
         {
             InitializeComponent();
         }
+
+        SoundPlayer Player;
+        int x = 1;
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -42,6 +48,35 @@ namespace Proyecto_Universtory_WPF
         {
             Window main = Window.GetWindow(this);
             main.Close();
+        }
+
+        private void FormLoad(object sender, RoutedEventArgs e)
+        {
+           var st = (Storyboard)Resources["Movimiento"];
+            st.Begin(btnStart);
+
+           var trivia = (Storyboard)Resources["Trivia"];
+            trivia.Begin(btnTrivia);
+
+            Player = new SoundPlayer
+            {
+                SoundLocation = "C:/Users/ALUMNO/Downloads/universtory_song.wav"
+            };
+            Player.Play(); 
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+             if (Player != null)
+             {
+                Player.Stop();
+             }
+            
+             else if (Player == null)
+             {
+                Player.Play();
+             }
+            
         }
     }
 }
