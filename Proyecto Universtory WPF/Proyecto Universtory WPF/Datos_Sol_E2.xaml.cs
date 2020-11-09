bd85021c29a4dd8f97fbace56062cad3854edeed
sanchezmartin09 <sanchez090904@gmail.com>
2020-11-09 01:56:01 -0300

    vents de etapas terminado

---
+++ Proyecto Universtory WPF/Proyecto Universtory WPF/Datos_Sol_E2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Universtory_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para Datos_Sol_E2.xaml
+    /// </summary>
+    public partial class Datos_Sol_E2 : Window
+    {
+        public Datos_Sol_E2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
